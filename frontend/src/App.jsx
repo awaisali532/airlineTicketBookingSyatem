dda1043f749abd999a4c3beea77a4dca3b1004c5
Awais Ali <awaisali5321@gmail.com>
2025-03-23 23:28:18 +0500

    offline bootstrap ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const App = () => {
-  return <div className="bg-danger text-white">App</div>;
+  return <div className="btn  m-2 p-3 bg-success-subtle">App</div>;
 };
 
 export default App;

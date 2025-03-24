2617c024675f5e5dc829db61eb53a3dd67a1c75c
Awais Ali <awaisali532193@gmail.com>
2025-03-24 07:17:57 +0500

    route more

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Checkin from "./pages/Checkin";
+import Confirmation from "./pages/Confirmation";
+import Contact from "./pages/Contact";
+import FlightDetails from "./pages/FlightDetails";
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/checkin" element={<Checkin/>} />
+        <Route path="confirmation" element={<Confirmation />} />"
+        <Route path="contactus" element={<Contact/>} />
+        <Route path="/flightdetails" element={<FlightDetails />} />
       </Routes>
     </div>
   );

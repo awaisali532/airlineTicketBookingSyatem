205d12bd3c985079601abc2aacef56a9872d98cf
Awais Ali <awaisali5321@gmail.com>
2025-03-24 15:24:08 +0500

    Add routese are added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,16 +8,22 @@ import Checkin from "./pages/Checkin";
 import Confirmation from "./pages/Confirmation";
 import Contact from "./pages/Contact";
 import FlightDetails from "./pages/FlightDetails";
+import FlightSearch from "./pages/FlightSearch";
+import FlightTracking from "./pages/FlightDetails";
+import Auth from "./pages/Auth";
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/checkin" element={<Checkin/>} />
+        <Route path="/checkin" element={<Checkin />} />
         <Route path="confirmation" element={<Confirmation />} />"
-        <Route path="contactus" element={<Contact/>} />
+        <Route path="contactus" element={<Contact />} />
         <Route path="/flightdetails" element={<FlightDetails />} />
+        <Route path="/flightsearch" element={<FlightSearch />} />
+        <Route path="/flighttracking" element={<FlightTracking />} />
+        <Route path="/login" element={<Auth />} />
       </Routes>
     </div>
   );
